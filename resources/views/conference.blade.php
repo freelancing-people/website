e0d76b4d5fa3532f88fa93be45eff9d91ad52cde
Pankaj Patil <pankajpatil822@gmail.com>
2017-06-10 20:47:18 +0530

    Favicon changed

--- resources/views/conference.blade.php
+++ resources/views/conference.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Meta for responsiveness -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- Meta for Edge style rendering in IE -->
-    <link rel="icon" type="image/png" href="{{asset('img/logo-favi.svg')}}">
+    <link rel='shortcut icon' href="{{asset('img/favicon.ico')}}" type='image/x-icon'>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Satisfy|Ubuntu" rel="stylesheet">
     <!-- External font - Open Sans -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
