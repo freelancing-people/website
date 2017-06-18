c08d20faf9ec49445d7f8492f9be426695e81ba5
Viraj Khatavkar <viraj.2438@gmail.com>
2017-06-18 12:40:19 +0530

    Changed image

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -15,14 +15,14 @@
     <meta property="og:url" content="https://freelancingpeople.com" />
     <meta property="og:title" content="Free E-mail Course: Turn your website into a Lead Magnet" />
     <meta property="og:description" content="Not getting enough lead from website? Join a free, 5 lesson course and learn how to generate a continuous pipeline of leads from your website." />
-    <meta property="og:image" content="{{asset('img/logo-color.png')}}" />
+    <meta property="og:image" content="{{asset('img/free-email-course.png')}}" />
 
     <!-— Twitter Cards -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:domain" value="https://freelancingpeople.com" />
     <meta name="twitter:title" value="Free E-mail Course: Turn your website into a Lead Magnet" />
     <meta name="twitter:description" value="Not getting enough lead from website? Join a free, 5 lesson course and learn how to generate a continuous pipeline of leads from your website." />
-    <meta name="twitter:image" content="{{asset('img/logo-color.png')}}" />
+    <meta name="twitter:image" content="{{asset('img/free-email-course.png')}}" />
     <meta name="twitter:url" value="http://freelancingpeople.com" />
     @include('includes.pixel')
 </head>
