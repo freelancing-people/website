35d81ed74811589e45544abadeef6f61778229b3
Pankaj Patil <pankajpatil822@gmail.com>
2017-07-02 22:57:51 +0530

    Articles page design completed

--- resources/views/website/articles/index.blade.php
+++ resources/views/website/articles/index.blade.php
@@ -2,30 +2,166 @@
 
 @section('content')
 
-	<section class="services-section article-show-section">
-		<div class="list-view">
-			<div class="blue-triangle"></div>
-			<div class="gray-triangle"></div>
-			<div class="list-header">
-				<h1>How Sean DʼSouza Changed Everything I now About Pricing</h1>
+	<section class="articles-section">
+		<div class="container">
+			<div class="articles-header">
+				<h1>"This blog looks so helpful. My face lit up like a kid's face after seeing an ice cream."</h1>
+				<ul class="list-unstyled list-inline text-center">
+					<li><a href="javascript:void(0)">Developing</a></li>
+					<li><a href="javascript:void(0)">Designing</a></li>
+					<li><a href="javascript:void(0)">Freelancing</a></li>
+					<li><a href="javascript:void(0)">Mixed Bag</a></li>
+				</ul>
 			</div>
-			<div class="list-body">
-				<div class="profile-info">
-					<div class="content">
-						<h3>Background and what are you working on.</h3>
-						<p>My name is Andrew Carpenter, and my background is in industrial/organizational psychology, which I studied at Colorado State, as well as business analytics, which I studied at CU-Boulder. My mission is to combine these two fields, the study of business data and the study of business people, to produce business processes that make people successful. </p>
-						<p>Intrinio, where I am the director of HR analytics, is giving me a chance to do just that. I joined Intrinio as employee number four in 2015. Since then, I have helped create the sales, support, HR, and marketing infrastructure the company needs to scale. Intrinio provides financial data and apps that analyze that data in a fintech marketplace. Our users are evenly split between developers that use our API (application programming interface) to build apps for investors, and investors who use those apps to make investment decisions. With the help of the internal systems we have developed, we've managed to scale to over 10,000 users, and we're currently onboarding 50-60 users per day. My hope is that, through this interview, I can shed some light on the business processes and tools that let us handle such high volumes affordably with a very small team. They've helped us grow from a startup doing $10,000 a month in revenue in 2016 to a scaling business whose users make 5 million API calls a day.</p>
-						<h3>Story behind how Intrinio got started.</h3>
-						<p>Joey French and Rachel Carpenter (my sister) founded Intrinio in 2012. They both had backgrounds in finance and thought there should be a way to automate financial analyses. They set off to build an app that would value a stock with the push of a button. However, when they went to financial data providers looking for data to power the app, the quotes came back at $50,000 a month. At the time they had no funding, so shelling out $600,000 a year just for data was impossible.</p>
-						<h2>“Our team has experienced all of the problems our platform solves.”</h2>
-						<h3>What convinced you to join</h3>
-						<p>Joey and Rachel were pissed off at data providers, and their anger rubbed off on me. There aren't a lot of apps out there that enable everyday people to make good investment decisions, because startup app developers can't get the data they need to build innovative tools. The result is that poor people can't learn to invest for themselves, because only a "professional" can afford the training and tools needed to understand the market. Since we already had the data, we ended up building the original automated investing app idea, but by 2015 the data was the focus, so we made the app free. Today, we keep the finan</p>
+			<div class="articles-body">
+				<div class="design-type-1">
+					<div class="container-fluid">
+						<div class="col-sm-7">
+							<div class="article-design-1">
+								<div class="design-box">
+									
+								</div>
+								<a href="javascript:void(0)" class="article-link"><h1>How Sean DʼSouza Changed Everything I Know About Pricing</h1></a>
+							</div>
+						</div>
+						<div class="col-sm-5">
+							<div class="article-info">
+								<p>Until yesterday, Iʼve been a confident user of a three-tier pricing strategy. But then Sean DʼSouza (author of my favorite book, The Brain Audit) came along with his talk at Double Your Freelancing Conference in Stockholm. His pricing method turned my world upside down. So Iʼm sharing his method with you!</p>
+								<a href="javascript:void(0)" class="read-more-btn">Read More</a>
+							</div>
+						</div>
 					</div>
-					<span class="line line-1"></span>
-					<span class="line line-2"></span>
-					<span class="line line-3"></span>
-					<span class="line line-4"></span>
 				</div>
+				<div class="design-type design-type-2">
+					<div class="container-fluid">
+						<div class="col-sm-6">
+							<div class="article-design-2">
+								<div class="design-box">
+									
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="article-info">
+								<a href="javascript:void(0)" class="article-link"><h1>How Sean DʼSouza Changed Everything I Know About Pricing</h1></a>
+								<p>Until yesterday, Iʼve been a confident user of a three-tier pricing strategy. But then Sean DʼSouza (author of my favorite book, The Brain Audit) came along with his talk at Double Your Freelancing Conference in Stockholm. His pricing method turned my world upside down. So Iʼm sharing his method with you!</p>
+								<a href="javascript:void(0)" class="read-more-btn">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="design-type design-type-3">
+					<div class="container-fluid">
+						<div class="col-sm-7">
+							<div class="article-info">
+								<a href="javascript:void(0)" class="article-link"><h1>How to Build a SaaS Product While Having Limited Technical Skills</h1></a>
+								<p>Being a SaaS founder is no easy feat if you donʼt possess necessary technical skills. Today Iʼm answering a question by Tobey Tozier, a designer from Portland, Maine: “Say you have a big idea for a SaaS project, but many of the skills needed to execute it are outside your expertise as a designer. Where do you start?”</p>
+								<a href="javascript:void(0)" class="read-more-btn">Read More</a>
+							</div>
+						</div>
+						<div class="col-sm-5">
+							<div class="article-design-3">
+								<div class="design-box">
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="design-type design-type-4">
+					<div class="container-fluid">
+						<div class="col-sm-12">
+							<div class="article-info">
+								<a href="javascript:void(0)" class="article-link"><h1>Should You Include Solutions or Mockups in Your Proposal?</h1></a>
+								<p>How do you win the heart of a new client? Should you do free work to quickly illustrate your skills? Hereʼs a question from Tiffany Teng, a UI/UX designer based in Malaysia and Singapore: “In order to win a client project, would you recommend us to include mockups (proposed solutions) in the proposal? And why?”</p>
+								<a href="javascript:void(0)" class="read-more-btn">Read More</a>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="article-design-4">
+								<div class="design-box">
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="design-type design-type-1">
+					<div class="container-fluid">
+						<div class="col-sm-7">
+							<div class="article-design-1">
+								<div class="design-box">
+									
+								</div>
+								<a href="javascript:void(0)" class="article-link"><h1>How Sean DʼSouza Changed Everything I Know About Pricing</h1></a>
+							</div>
+						</div>
+						<div class="col-sm-5">
+							<div class="article-info">
+								<p>Until yesterday, Iʼve been a confident user of a three-tier pricing strategy. But then Sean DʼSouza (author of my favorite book, The Brain Audit) came along with his talk at Double Your Freelancing Conference in Stockholm. His pricing method turned my world upside down. So Iʼm sharing his method with you!</p>
+								<a href="javascript:void(0)" class="read-more-btn">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="design-type design-type-2">
+					<div class="container-fluid">
+						<div class="col-sm-6">
+							<div class="article-design-2">
+								<div class="design-box">
+									
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="article-info">
+								<a href="javascript:void(0)" class="article-link"><h1>How Sean DʼSouza Changed Everything I Know About Pricing</h1></a>
+								<p>Until yesterday, Iʼve been a confident user of a three-tier pricing strategy. But then Sean DʼSouza (author of my favorite book, The Brain Audit) came along with his talk at Double Your Freelancing Conference in Stockholm. His pricing method turned my world upside down. So Iʼm sharing his method with you!</p>
+								<a href="javascript:void(0)" class="read-more-btn">Read More</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="design-type design-type-3">
+					<div class="container-fluid">
+						<div class="col-sm-7">
+							<div class="article-info">
+								<a href="javascript:void(0)" class="article-link"><h1>How to Build a SaaS Product While Having Limited Technical Skills</h1></a>
+								<p>Being a SaaS founder is no easy feat if you donʼt possess necessary technical skills. Today Iʼm answering a question by Tobey Tozier, a designer from Portland, Maine: “Say you have a big idea for a SaaS project, but many of the skills needed to execute it are outside your expertise as a designer. Where do you start?”</p>
+								<a href="javascript:void(0)" class="read-more-btn">Read More</a>
+							</div>
+						</div>
+						<div class="col-sm-5">
+							<div class="article-design-3">
+								<div class="design-box">
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="design-type design-type-4">
+					<div class="container-fluid">
+						<div class="col-sm-12">
+							<div class="article-info">
+								<a href="javascript:void(0)" class="article-link"><h1>Should You Include Solutions or Mockups in Your Proposal?</h1></a>
+								<p>How do you win the heart of a new client? Should you do free work to quickly illustrate your skills? Hereʼs a question from Tiffany Teng, a UI/UX designer based in Malaysia and Singapore: “In order to win a client project, would you recommend us to include mockups (proposed solutions) in the proposal? And why?”</p>
+								<a href="javascript:void(0)" class="read-more-btn">Read More</a>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="article-design-4">
+								<div class="design-box">
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<span class="line line-1"></span>
+				<span class="line line-2"></span>
+				<span class="line line-3"></span>
+				<span class="line line-4"></span>
 			</div>
 		</div>
 	</section>
