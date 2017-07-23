1765ee90f0259c134d45cd2788ae2e61361d5d8b
Viraj Khatavkar <viraj.2438@gmail.com>
2017-07-23 13:48:35 -0400

    Updated the prices

--- resources/views/conference.blade.php
+++ resources/views/conference.blade.php
@@ -2000,7 +2000,7 @@
                             <span class="display-block font-family-alt letter-spacing-1 text-gray-dark-2 text-large text-uppercase">Early Bird</span>
 
                             <span class="display-block font-family-alt margin-11 no-letter-spacing no-margin-bottom no-margin-rl text-gray-dark-2 title-extra-large-4">
-                                    <span class="pricing-dollar position-relative title-large">$</span>25
+                                    <span class="pricing-dollar position-relative title-large">$</span>50
                                 </span>
 
                             <span class="bg-gray-light-2 margin-10-5 no-margin-bottom no-margin-rl separator-line-full"></span>
@@ -2016,8 +2016,8 @@
                             </ul>
 
                             <p class="margin-11 no-margin-bottom no-margin-rl text-gray-dark-2 text-medium">Ticket
-                                prices are $25.00 if you reserve by 15th July, otherwise general admission will cost
-                                $50.00.</p>
+                                prices are $50.00 if you reserve by 30th August, otherwise general admission will cost
+                                $75.00.</p>
                         </div>
                         <!-- //.display-table-cell -->
                     </div>
