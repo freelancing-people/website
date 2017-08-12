343bbcbd2872578594d78b9ac2f0247c67c0eced
Viraj Khatavkar <viraj.2438@gmail.com>
2017-08-12 15:28:26 -0400

    Added analytics and pixel tracking

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -13,6 +13,8 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}"> <!-- Compiled css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
 
+    @include('includes.pixel')
+
 </head>
 <body>
 <nav class="navbar navbar-default">
