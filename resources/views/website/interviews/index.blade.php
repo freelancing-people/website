20cb861e88e94222451a5a75f24ca75bc5b46903
Viraj Khatavkar <viraj.2438@gmail.com>
2017-08-12 15:26:19 -0400

    Added image for social sharing on homepage

--- resources/views/website/interviews/index.blade.php
+++ resources/views/website/interviews/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('meta')
     <meta property="og:description" content="Learn from profitable freelancers and agencies">
-    <meta property="og:image" content="/images/freelancing-people-shareable.png">
+    <meta property="og:image" content="https://freelancingpeople.com/images/freelancing-people-shareable.png">
 
     <meta property="og:image:height" content="420">
     <meta property="og:image:width" content="840">
@@ -13,7 +13,7 @@
 
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:description" content="Learn from profitable freelancers and agencies">
-    <meta name="twitter:image" content="/images/freelancing-people-shareable.png">
+    <meta name="twitter:image" content="https://freelancingpeople.com/images/freelancing-people-shareable.png">
     <meta name="twitter:site" content="@freelancingclub">
     <meta name="twitter:title" content="Freelancing People">
 @endsection
